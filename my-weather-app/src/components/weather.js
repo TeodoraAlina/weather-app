82a964c29e35a74177335c7cca3d5d80bd660329
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-07-10 23:08:35 +0200

    Integrate Weather component into app.js for weather data display

---
+++ my-weather-app/src/components/weather.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Card } from 'semantic-ui-react'
+
+const CardExampleCard = ({weatherData}) => (
+  <Card>
+    <Card.Content>
+        <Card.Header className="header">City Name: {weatherData.name}</Card.Header>
+        <p>Temperature: {weatherData.main.temp}</p>
+        <p>Sunrise: {weatherData.sys.sunrise}</p>
+        <p>Sunset: {weatherData.sys.sunset}</p>
+        <p>Description: {weatherData.weather[0].description}</p>
+    </Card.Content>
+  </Card>
+)
+
+export default CardExampleCard;

612095f407996366d2575442a4f6c2f0392029fb
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-07-10 22:39:16 +0200

    Add geolocation fetching using useState and useEffect hooks

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is:", lat)
+    console.log("Longitude is:", long)
+  }, [lat, long]);
+
   return (
     <div className="App">
-      
+
     </div>
   );
-}
 
-export default App;
+}

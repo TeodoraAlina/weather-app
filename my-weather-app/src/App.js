70ba67b08ff72011de6edcb393ddb98635b4f1d0
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-07-15 07:35:31 +0200

    Add refresh button and loading spinner

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
 import Weather from './components/weather'
+import { Dimmer, Loader } from 'semantic-ui-react';
 
 export default function App() {
 
@@ -30,7 +31,11 @@ export default function App() {
       {(typeof data.main != 'undefined') ? (
         <Weather weatherData={data}/>
       ) : (
-        <div></div>
+        <div>
+          <Dimmer active>
+            <Loader>Loading..</Loader>
+          </Dimmer>
+        </div>
       )}
 
     </div>
--- my-weather-app/src/components/weather.js
+++ my-weather-app/src/components/weather.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import './styles.css';
 import moment from 'moment';
+import { Button } from 'semantic-ui-react';
+
+const refresh = () => {
+    window.location.reload();
+}
 
 const WeatherCard = ({weatherData}) => (
   <div className="main">
-      <p className="header">{weatherData.name}</p>
+    <div className="top">
+    <p className="header">{weatherData.name}</p>
+      <Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} />
+    </div>
       <div className="flex">
         <p className="day">{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
         <p className="description">{weatherData.weather[0].main}</p>
       </div>
 
       <div className="flex">
-        <p className="temp">Temprature: {weatherData.main.temp} &deg;C</p>
+        <p className="temp">Temperature: {weatherData.main.temp} &deg;C</p>
         <p className="temp">Humidity: {weatherData.main.humidity} %</p>
       </div>
 
